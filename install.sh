ca708ecf2e67804aaedbc97875aaf6ea6e8d7dab
Hepheir <hepheir@gmail.com>
2025-08-13 17:28:41 +0900

    feat: add `zsh-autosuggestions` (CLI 자동완성)

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rm ~/.zshrc
 ln -s $(dirname $(realpath $0))/.zshrc ~/.zshrc
+
+# Install zsh-autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
